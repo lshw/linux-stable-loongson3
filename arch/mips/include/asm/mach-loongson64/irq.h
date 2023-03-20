FlyGoat/linux-stable

Loongson: Add basic support for LS7A bridge
Signed-off-by: Binbin Zhou <zhoubb@lemote.com>

arch/mips/include/asm/mach-loongson64/irq.h
@@ -104,6 +104,57 @@
 #define LS2H_PCH_GPIO15_IRQ		(LS2H_PCH_IRQ_BASE + 79)
 #define LS2H_PCH_LAST_IRQ		(LS2H_PCH_IRQ_BASE + 160)
 
+#define LS7A_PCH_IRQ_BASE		64
+#define LS7A_PCH_UART0_IRQ		(LS7A_PCH_IRQ_BASE + 8)
+#define LS7A_PCH_I2C0_IRQ		(LS7A_PCH_IRQ_BASE + 9)
+#define LS7A_PCH_GMAC0_SBD_IRQ		(LS7A_PCH_IRQ_BASE + 12)
+#define LS7A_PCH_GMAC0_PMT_IRQ		(LS7A_PCH_IRQ_BASE + 13)
+#define LS7A_PCH_GMAC1_SBD_IRQ		(LS7A_PCH_IRQ_BASE + 14)
+#define LS7A_PCH_GMAC1_PMT_IRQ		(LS7A_PCH_IRQ_BASE + 15)
+#define LS7A_PCH_SATA0_IRQ		(LS7A_PCH_IRQ_BASE + 16)
+#define LS7A_PCH_SATA1_IRQ		(LS7A_PCH_IRQ_BASE + 17)
+#define LS7A_PCH_SATA2_IRQ		(LS7A_PCH_IRQ_BASE + 18)
+#define LS7A_PCH_LPC_IRQ		(LS7A_PCH_IRQ_BASE + 19)
+#define LS7A_PCH_PWM0_IRQ		(LS7A_PCH_IRQ_BASE + 24)
+#define LS7A_PCH_PWM1_IRQ		(LS7A_PCH_IRQ_BASE + 25)
+#define LS7A_PCH_PWM2_IRQ		(LS7A_PCH_IRQ_BASE + 26)
+#define LS7A_PCH_PWM3_IRQ		(LS7A_PCH_IRQ_BASE + 27)
+#define LS7A_PCH_DC_IRQ			(LS7A_PCH_IRQ_BASE + 28)
+#define LS7A_PCH_GPU_IRQ		(LS7A_PCH_IRQ_BASE + 29)
+#define LS7A_PCH_PCIE_F0_PORT0_IRQ	(LS7A_PCH_IRQ_BASE + 32)
+#define LS7A_PCH_PCIE_F0_PORT1_IRQ	(LS7A_PCH_IRQ_BASE + 33)
+#define LS7A_PCH_PCIE_F0_PORT2_IRQ	(LS7A_PCH_IRQ_BASE + 34)
+#define LS7A_PCH_PCIE_F0_PORT3_IRQ	(LS7A_PCH_IRQ_BASE + 35)
+#define LS7A_PCH_PCIE_F1_PORT0_IRQ	(LS7A_PCH_IRQ_BASE + 36)
+#define LS7A_PCH_PCIE_F1_PORT1_IRQ	(LS7A_PCH_IRQ_BASE + 37)
+#define LS7A_PCH_PCIE_H_LO_IRQ		(LS7A_PCH_IRQ_BASE + 38)
+#define LS7A_PCH_PCIE_H_HI_IRQ		(LS7A_PCH_IRQ_BASE + 39)
+#define LS7A_PCH_PCIE_G0_LO_IRQ		(LS7A_PCH_IRQ_BASE + 40)
+#define LS7A_PCH_PCIE_G0_HI_IRQ		(LS7A_PCH_IRQ_BASE + 41)
+#define LS7A_PCH_PCIE_G1_LO_IRQ		(LS7A_PCH_IRQ_BASE + 42)
+#define LS7A_PCH_PCIE_G1_HI_IRQ		(LS7A_PCH_IRQ_BASE + 43)
+#define LS7A_PCH_TOY0_IRQ		(LS7A_PCH_IRQ_BASE + 44)
+#define LS7A_PCH_TOY1_IRQ		(LS7A_PCH_IRQ_BASE + 45)
+#define LS7A_PCH_TOY2_IRQ		(LS7A_PCH_IRQ_BASE + 46)
+#define LS7A_PCH_ACPI_IRQ		(LS7A_PCH_IRQ_BASE + 47)
+#define LS7A_PCH_EHCI0_IRQ		(LS7A_PCH_IRQ_BASE + 48)
+#define LS7A_PCH_OHCI0_IRQ		(LS7A_PCH_IRQ_BASE + 49)
+#define LS7A_PCH_EHCI1_IRQ		(LS7A_PCH_IRQ_BASE + 50)
+#define LS7A_PCH_OHCI1_IRQ		(LS7A_PCH_IRQ_BASE + 51)
+#define LS7A_PCH_RTC_INT0_IRQ		(LS7A_PCH_IRQ_BASE + 52)
+#define LS7A_PCH_RTC_INT1_IRQ		(LS7A_PCH_IRQ_BASE + 53)
+#define LS7A_PCH_RTC_INT2_IRQ		(LS7A_PCH_IRQ_BASE + 54)
+#define LS7A_PCH_HPET_IRQ		(LS7A_PCH_IRQ_BASE + 55)
+#define LS7A_PCH_AC97_DMA0_IRQ		(LS7A_PCH_IRQ_BASE + 56)
+#define LS7A_PCH_AC97_DMA1_IRQ		(LS7A_PCH_IRQ_BASE + 57)
+#define LS7A_PCH_AC97_HDA_IRQ		(LS7A_PCH_IRQ_BASE + 58)
+#define LS7A_PCH_GPIO_HI_IRQ		(LS7A_PCH_IRQ_BASE + 59)
+#define LS7A_PCH_GPIO_0_IRQ		(LS7A_PCH_IRQ_BASE + 60)
+#define LS7A_PCH_GPIO_1_IRQ		(LS7A_PCH_IRQ_BASE + 61)
+#define LS7A_PCH_GPIO_2_IRQ		(LS7A_PCH_IRQ_BASE + 62)
+#define LS7A_PCH_GPIO_3_IRQ		(LS7A_PCH_IRQ_BASE + 63)
+#define LS7A_PCH_LAST_IRQ		(LS2H_PCH_IRQ_BASE + 64)
+
 #define RS780_PCH_HPET_IRQ		0
 #define RS780_PCH_ACPI_IRQ		7
 

lshw/linux-stable-loongson3

arch/mips/include/asm/mach-loongson64/irq.h
@@ -108,6 +108,57 @@
 #define LS2H_PCH_GPIO15_IRQ		(LS2H_PCH_IRQ_BASE + 79)
 #define LS2H_PCH_LAST_IRQ		(LS2H_PCH_IRQ_BASE + 160)
 
+#define LS7A_PCH_IRQ_BASE		64
+#define LS7A_PCH_UART0_IRQ		(LS7A_PCH_IRQ_BASE + 8)
+#define LS7A_PCH_I2C0_IRQ		(LS7A_PCH_IRQ_BASE + 9)
+#define LS7A_PCH_GMAC0_SBD_IRQ		(LS7A_PCH_IRQ_BASE + 12)
+#define LS7A_PCH_GMAC0_PMT_IRQ		(LS7A_PCH_IRQ_BASE + 13)
+#define LS7A_PCH_GMAC1_SBD_IRQ		(LS7A_PCH_IRQ_BASE + 14)
+#define LS7A_PCH_GMAC1_PMT_IRQ		(LS7A_PCH_IRQ_BASE + 15)
+#define LS7A_PCH_SATA0_IRQ		(LS7A_PCH_IRQ_BASE + 16)
+#define LS7A_PCH_SATA1_IRQ		(LS7A_PCH_IRQ_BASE + 17)
+#define LS7A_PCH_SATA2_IRQ		(LS7A_PCH_IRQ_BASE + 18)
+#define LS7A_PCH_LPC_IRQ		(LS7A_PCH_IRQ_BASE + 19)
+#define LS7A_PCH_PWM0_IRQ		(LS7A_PCH_IRQ_BASE + 24)
+#define LS7A_PCH_PWM1_IRQ		(LS7A_PCH_IRQ_BASE + 25)
+#define LS7A_PCH_PWM2_IRQ		(LS7A_PCH_IRQ_BASE + 26)
+#define LS7A_PCH_PWM3_IRQ		(LS7A_PCH_IRQ_BASE + 27)
+#define LS7A_PCH_DC_IRQ			(LS7A_PCH_IRQ_BASE + 28)
+#define LS7A_PCH_GPU_IRQ		(LS7A_PCH_IRQ_BASE + 29)
+#define LS7A_PCH_PCIE_F0_PORT0_IRQ	(LS7A_PCH_IRQ_BASE + 32)
+#define LS7A_PCH_PCIE_F0_PORT1_IRQ	(LS7A_PCH_IRQ_BASE + 33)
+#define LS7A_PCH_PCIE_F0_PORT2_IRQ	(LS7A_PCH_IRQ_BASE + 34)
+#define LS7A_PCH_PCIE_F0_PORT3_IRQ	(LS7A_PCH_IRQ_BASE + 35)
+#define LS7A_PCH_PCIE_F1_PORT0_IRQ	(LS7A_PCH_IRQ_BASE + 36)
+#define LS7A_PCH_PCIE_F1_PORT1_IRQ	(LS7A_PCH_IRQ_BASE + 37)
+#define LS7A_PCH_PCIE_H_LO_IRQ		(LS7A_PCH_IRQ_BASE + 38)
+#define LS7A_PCH_PCIE_H_HI_IRQ		(LS7A_PCH_IRQ_BASE + 39)
+#define LS7A_PCH_PCIE_G0_LO_IRQ		(LS7A_PCH_IRQ_BASE + 40)
+#define LS7A_PCH_PCIE_G0_HI_IRQ		(LS7A_PCH_IRQ_BASE + 41)
+#define LS7A_PCH_PCIE_G1_LO_IRQ		(LS7A_PCH_IRQ_BASE + 42)
+#define LS7A_PCH_PCIE_G1_HI_IRQ		(LS7A_PCH_IRQ_BASE + 43)
+#define LS7A_PCH_TOY0_IRQ		(LS7A_PCH_IRQ_BASE + 44)
+#define LS7A_PCH_TOY1_IRQ		(LS7A_PCH_IRQ_BASE + 45)
+#define LS7A_PCH_TOY2_IRQ		(LS7A_PCH_IRQ_BASE + 46)
+#define LS7A_PCH_ACPI_IRQ		(LS7A_PCH_IRQ_BASE + 47)
+#define LS7A_PCH_EHCI0_IRQ		(LS7A_PCH_IRQ_BASE + 48)
+#define LS7A_PCH_OHCI0_IRQ		(LS7A_PCH_IRQ_BASE + 49)
+#define LS7A_PCH_EHCI1_IRQ		(LS7A_PCH_IRQ_BASE + 50)
+#define LS7A_PCH_OHCI1_IRQ		(LS7A_PCH_IRQ_BASE + 51)
+#define LS7A_PCH_RTC_INT0_IRQ		(LS7A_PCH_IRQ_BASE + 52)
+#define LS7A_PCH_RTC_INT1_IRQ		(LS7A_PCH_IRQ_BASE + 53)
+#define LS7A_PCH_RTC_INT2_IRQ		(LS7A_PCH_IRQ_BASE + 54)
+#define LS7A_PCH_HPET_IRQ		(LS7A_PCH_IRQ_BASE + 55)
+#define LS7A_PCH_AC97_DMA0_IRQ		(LS7A_PCH_IRQ_BASE + 56)
+#define LS7A_PCH_AC97_DMA1_IRQ		(LS7A_PCH_IRQ_BASE + 57)
+#define LS7A_PCH_AC97_HDA_IRQ		(LS7A_PCH_IRQ_BASE + 58)
+#define LS7A_PCH_GPIO_HI_IRQ		(LS7A_PCH_IRQ_BASE + 59)
+#define LS7A_PCH_GPIO_0_IRQ		(LS7A_PCH_IRQ_BASE + 60)
+#define LS7A_PCH_GPIO_1_IRQ		(LS7A_PCH_IRQ_BASE + 61)
+#define LS7A_PCH_GPIO_2_IRQ		(LS7A_PCH_IRQ_BASE + 62)
+#define LS7A_PCH_GPIO_3_IRQ		(LS7A_PCH_IRQ_BASE + 63)
+#define LS7A_PCH_LAST_IRQ		(LS7A_PCH_IRQ_BASE + 64)
+
 #define RS780_PCH_HPET_IRQ		0
 #define RS780_PCH_ACPI_IRQ		7
 